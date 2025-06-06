91e65f57b50d27b5d615a03e5190db08cedc8ee0
svanur <svanur.karlsson@gmail.com>
2025-06-06 19:24:11 +0000

    Add data.ts with static material, will be replaced later on.
    Update TrailDetails page, displays content about selected trail

--- TrailFinder.Web/src/App.tsx
+++ TrailFinder.Web/src/App.tsx
@@ -1,16 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Layout from './components/layout/Layout';
+import TrailDetails from './pages/TrailDetails';
 
 function App() {
     return (
         <Router>
-            <Layout>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    {/* Add more routes here later */}
-                </Routes>
-            </Layout>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/run/:normalizedName" element={<TrailDetails />} />
+            </Routes>
         </Router>
     );
 }
--- TrailFinder.Web/src/components/layout/Footer.tsx
+++ TrailFinder.Web/src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="bg-gray-100 mt-8">
             <div className="container mx-auto px-4 py-6">
                 <p className="text-center text-gray-600">
-                    © {new Date().getFullYear()} Hlaupaleiðir. All rights reserved.
+                    © {new Date().getFullYear()} Hlaupaleiðir. Allur réttur áskilinn.
                 </p>
             </div>
         </footer>
