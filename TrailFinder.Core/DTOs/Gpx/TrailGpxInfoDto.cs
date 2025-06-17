06a1cabaa57fc5eec8f10914f203d5c6e1e1712c
svanur <svanur.karlsson@gmail.com>
2025-06-17 19:32:37 +0000

    Delete GeoPoint from GpxService

--- TrailFinder.Core/DTOs/Gpx/TrailGpxInfoDto.cs
+++ TrailFinder.Core/DTOs/Gpx/TrailGpxInfoDto.cs
@@ -3,7 +3,7 @@ namespace TrailFinder.Core.DTOs.Gpx;
 public record TrailGpxInfoDto(
     double DistanceMeters,
     double ElevationGainMeters,
-    GeoPoint StartPoint,
-    GeoPoint EndPoint
+    GpxPoint StartPoint,
+    GpxPoint EndPoint
 );
 
