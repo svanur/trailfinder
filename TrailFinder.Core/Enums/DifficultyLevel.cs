ee69ba14fadeb58e58648357a81a9513e2a998a5
svanur <svanur.karlsson@gmail.com>
2025-06-23 10:25:31 +0000

    Add http-client.env.json
    Add PgName attributes to DifficultyLevel enum

--- TrailFinder.Core/Enums/DifficultyLevel.cs
+++ TrailFinder.Core/Enums/DifficultyLevel.cs
@@ -1,11 +1,21 @@
+using NpgsqlTypes;
+
 namespace TrailFinder.Core.Enums;
 
 public enum DifficultyLevel
 {
+    [PgName("easy")]
     Easy,
+    
+    [PgName("moderate")]
     Moderate,
+    
+    [PgName("hard")]
     Hard,
+    
+    [PgName("extreme")]
     Extreme,
+    
+    [PgName("unknown")]
     Unknown
-
 }
