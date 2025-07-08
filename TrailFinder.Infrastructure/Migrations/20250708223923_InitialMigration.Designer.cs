46ea02ab550718064f54b0e619fc7a53b2fd8f84
svanur <svanur.karlsson@gmail.com>
2025-07-08 22:42:58 +0000

    Renew EF CORE migration files

--- TrailFinder.Infrastructure/Migrations/20250708223923_InitialMigration.Designer.cs
+++ TrailFinder.Infrastructure/Migrations/20250708223923_InitialMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -13,9 +14,11 @@ using TrailFinder.Infrastructure.Persistence;
 namespace TrailFinder.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250708223923_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TrailFinder.Infrastructure/Migrations/20250708223923_InitialMigration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+using TrailFinder.Core.Enums;
+
+#nullable disable
+
+namespace TrailFinder.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // The database schema is managed by Supabase, so no schema changes here.
+            // This migration solely exists to mark the current model state in __EFMigrationsHistory.
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // No schema changes to revert, as Up() did not create any.
+        }
+    }
+}
