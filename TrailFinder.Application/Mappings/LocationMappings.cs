f17e0b202a27e1ba7538948e53d57ef92a34367d
Svanur Karlsson <svanur.karlsson@gmail.com>
2025-07-15 17:25:01 +0000

    comment out mapping

--- TrailFinder.Application/Mappings/LocationMappings.cs
+++ TrailFinder.Application/Mappings/LocationMappings.cs
@@ -26,11 +26,13 @@ public class LocationMappings : Profile
                 opt => opt.MapFrom(src => src.Id)
             )
     */
+  /*
             .ForMember(
                 dest => dest.GpxPoint,
                 opt 
                     => opt.MapFrom(src => new GpxPoint(src.Latitude, src.Longitude, 0))
             )
+            */
             ;
 
         // List to PaginatedResult mapping
